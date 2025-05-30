e95b3e44c77ccd6b8210629a430fb544a662139d
Fawole John O <jattyro@gmail.com>
2025-05-30 17:45:49 -0400

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const tournamentRoutes = require('./routes/tournaments');
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 // Middleware
 app.use(cors());
